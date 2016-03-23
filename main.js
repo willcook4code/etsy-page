887c5b4175a432dc87586ab0cb189e02f73a1e23
Will Cook <wellsee83@gmail.com>
2016-03-22 21:09:32 -0500

    Some styling

--- main.js
+++ main.js
@@ -6,14 +6,13 @@ var results = {
 		console.log(data);
 		var container = $('#searchResults');
 		data.results.forEach(function(val, i, arr) {
-			var outerBox = $('<div></div>');
-			var imageBox = $('<img>', {'src': val.Images[0].url_75x75});
+			var outerBox = $('<div class="outerBox"></div>');
+			var imageBox = $('<img>', {'src': val.Images[0].url_170x135});
 			var imageLink = $('<a></a>', {href: val.url})
-			var displayTitle = $('<div></div>');
+			var displayTitle = $('<div class="titles"></div>');
 			var titleLink = $('<a></a>', {href: val.url}).html(val.title);
-			var seller = $('<a></a>', {href: val.Shop.url}).html(val.Shop.shop_name);
-			var price = $('<span></span>').html('$' +val.price);
-
+			var seller = $('<a></a>', {href: val.Shop.url, class: 'seller'}).html(val.Shop.shop_name);
+			var price = $('<span></span>', {class: 'price'}).html('$' +val.price);
 
 			imageLink.append(imageBox);
 			displayTitle.append(titleLink);
@@ -22,9 +21,9 @@ var results = {
 			outerBox.append(displayTitle);
 			outerBox.append(seller); 
 			outerBox.append(price);
+
 			container.append(outerBox);
 		});
-
 	},
 	error: function(err) {
 		console.log(err);
